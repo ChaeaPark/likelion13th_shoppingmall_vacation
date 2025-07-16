e27dec929bdba86e8be0987cf9c33a9720d75796
이정인 <jungingood32@gmail.com>
2025-07-16 18:14:15 +0900

    fix : api 통신(products)확인

--- src/apis/products.js
+++ src/apis/products.js
@@ -1,8 +1,10 @@
-import axios from 'axios';
+import instance from './axios';
 
 export const getAllProducts = async () => {
   try {
-    const response = await axios.get('/products');
+    // instance를 사용하여 상대 경로로 요청합니다.
+    const response = await instance.get('/api/v1/products');
+    console.log('상품 전체 조회 성공:', response.data);
     return response.data.products;
   } catch (error) {
     console.error('상품 전체 조회 실패:', error);
@@ -11,6 +13,6 @@ export const getAllProducts = async () => {
 };
 
 export const getProductById = async (id) => {
-  const response = await axios.get(`/products/${id}`);
+  const response = await isntance.get(`/products/${id}`);
   return response.data;
 };
