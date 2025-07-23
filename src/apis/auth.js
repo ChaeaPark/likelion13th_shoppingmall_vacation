d970a060eda084f2494f59872edfe3f8964e8b6d
이정인 <jungingood32@gmail.com>
2025-07-23 11:14:26 +0900

    fix : getCurrentUser 함수 추가

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -7,3 +7,8 @@ export const login = async (code) => {
 
   return res.data;
 };
+
+export const getCurrentUser = async () => {
+  const res = await instance.get('/auth/me'); // 실제 엔드포인트에 따라 수정 필요
+  return res.data;
+};
